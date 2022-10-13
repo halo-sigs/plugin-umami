b1ed42a77cb22c8a894f9a43cac030cd8d169cab
Ryan Wang <i@ryanc.cc>
2022-10-13 18:43:52 +0800

    feat: add UmamiTrackerProcessor
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ src/main/java/run/halo/umami/UmamiTrackerProcessor.java
@@ -0,0 +1,45 @@
+package run.halo.umami;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IModel;
+import org.thymeleaf.model.IModelFactory;
+import org.thymeleaf.processor.element.IElementModelStructureHandler;
+import reactor.core.publisher.Mono;
+import run.halo.app.plugin.SettingFetcher;
+import run.halo.app.theme.dialect.TemplateHeadProcessor;
+
+@Component
+public class UmamiTrackerProcessor implements TemplateHeadProcessor {
+
+    private final SettingFetcher settingFetcher;
+
+    public UmamiTrackerProcessor(SettingFetcher settingFetcher) {
+        this.settingFetcher = settingFetcher;
+    }
+
+    @Override
+    public Mono<Void> process(ITemplateContext context, IModel model,
+                              IElementModelStructureHandler structureHandler) {
+        return settingFetcher.fetch("basic", BasicConfig.class)
+                .map(basicConfig -> {
+                    final IModelFactory modelFactory = context.getModelFactory();
+                    model.add(modelFactory.createText(trackerScript(basicConfig.getWebsiteId(), basicConfig.endpoint)));
+                    return Mono.empty();
+                }).orElse(Mono.empty()).then();
+    }
+
+    private String trackerScript(String websiteId, String endpoint) {
+        return """
+                <script async defer data-website-id="%s" src="%s/umami.js"></script>
+                """.formatted(websiteId, endpoint);
+    }
+
+    @Data
+    public static class BasicConfig {
+        String websiteId;
+        String endpoint;
+        String url;
+    }
+}
