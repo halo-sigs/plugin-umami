1e55d9681f6de82e932266f78b678410479f7e06
Ryan Wang <i@ryanc.cc>
2022-10-26 15:17:57 +0800

    chore: bump dependencies

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,4 +1,4 @@
-import { definePlugin, BasicLayout } from "@halo-dev/console-shared";
+import { definePlugin } from "@halo-dev/console-shared";
 import UmamiView from "./views/UmamiView.vue";
 import MaterialSymbolsAnalyticsOutline from "~icons/material-symbols/analytics-outline";
 import { markRaw } from "vue";
@@ -8,27 +8,27 @@ export default definePlugin({
   components: [],
   routes: [
     {
-      path: "/umami",
-      component: BasicLayout,
-      children: [
-        {
-          path: "",
-          name: "Umami",
-          component: UmamiView,
-        },
-      ],
-    },
-  ],
-  menus: [
-    {
-      name: "统计",
-      items: [
-        {
-          name: "Umami",
-          path: "/umami",
-          icon: markRaw(MaterialSymbolsAnalyticsOutline),
-        },
-      ],
+      parentName: "Root",
+      route: {
+        path: "/umami",
+        children: [
+          {
+            path: "",
+            name: "Umami",
+            component: UmamiView,
+            meta: {
+              title: "Umami",
+              searchable: true,
+              menu: {
+                name: "Umami",
+                group: "tool",
+                icon: markRaw(MaterialSymbolsAnalyticsOutline),
+                priority: 0,
+              },
+            },
+          },
+        ],
+      },
     },
   ],
   extensionPoints: {},
